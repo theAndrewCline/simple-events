8206044c8258c2656315da6a1ecba269ae405da4
Andrew <acline@ariglobalsolutions.com>
2019-11-13 22:43:09 -0600

    a start on the modal

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -4,8 +4,8 @@ import '../css/Modal.css'
 export const Modal = ({ visable, toggleModal }: { visable: boolean, items?: any[], toggleModal: any }) => {
   if (visable) {
     return (
-      <div className='fixed pin z-50 overflow-auto bg-color-smoke flex'>
-        <div className='relative p-8 bg-white w-full max-w-md m-auto flex-col flex'>
+      <div className='fixed top-0 h-full w-full flex justify-center items-center modal'>
+        <div className="bg-gray-200 rounded h-64 w-full flex justify-center items-center flex-col m-64 mt-64">
           <h1>Hi I'm a modal</h1>
           <button className='btn btn-blue' onClick={() => toggleModal(false)}>Add</button>
         </div>
